f9f264c9c7d294f40b574d9e5cf6d404b2976620
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2022-04-04 10:04:31 +0900

    add controllable camera

--- src/context.h
+++ src/context.h
@@ -10,21 +10,39 @@
 
 CLASS_PTR(Context)
 class Context {
- public:
-  static ContextUPtr Create();
-  void Render();
+   public:
+    static ContextUPtr Create();
+    void Render();
+    void ProcessInput(GLFWwindow* window);
+    void MouseButton(int button, int action, double x, double y);
+    void MouseMove(double x, double y);
+    void Reshape(const int width, const int height);
 
- private:
-  Context() {}
-  bool Init();
-  ProgramUPtr m_program;
+   private:
+    Context() {}
+    bool Init();
+    ProgramUPtr m_program;
 
-  VertexLayoutUPtr m_vertexLayout;
-  BufferUPtr m_vertexBuffer;
-  BufferUPtr m_indexBuffer;
+    int m_width{WINDOW_WIDTH};
+    int m_height{WINDOW_HEIGHT};
 
-  TextureUPtr m_texture;
-  TextureUPtr m_texture2;
+    VertexLayoutUPtr m_vertexLayout;
+    BufferUPtr m_vertexBuffer;
+    BufferUPtr m_indexBuffer;
+
+    TextureUPtr m_texture;
+    TextureUPtr m_texture2;
+
+    // camera parameters
+    bool m_cameraControl {false};
+    glm::vec2 m_prevMousePos {glm::vec2(0.0f)};
+
+    glm::vec3 m_cameraPos{glm::vec3(0.0f, 0.0f, 3.0f)};
+    glm::vec3 m_cameraFront{glm::vec3(0.0f, 0.0f, -1.0f)};
+    glm::vec3 m_cameraUp{glm::vec3(0.0f, 1.0f, 0.0f)};
+
+    float m_cameraPitch {0.0f};
+    float m_cameraYaw {0.0f};
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,81 +3,99 @@
 
 void OnFramebufferSizeChanged(GLFWwindow *window, const int width,
                               const int height) {
-  SPDLOG_INFO("Framebuffer size changed: ({} x {})", width, height);
-  glViewport(0, 0, width, height);
+    SPDLOG_INFO("Framebuffer size changed: ({} x {})", width, height);
+    auto context = reinterpret_cast<Context *>(glfwGetWindowUserPointer(window));
+    context->Reshape(width, height);
 }
 
 void OnKeyEvent(GLFWwindow *window, const int key, const int scanCode,
                 const int action, const int mods) {
-  SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}", key, scanCode,
-              action == GLFW_PRESS     ? "Pressed"
-              : action == GLFW_RELEASE ? "Released"
-              : action == GLFW_REPEAT  ? "Repeat"
-                                       : "Unknown",
-              mods & GLFW_MOD_CONTROL ? "C" : "-",
-              mods & GLFW_MOD_SHIFT ? "S" : "-",
-              mods & GLFW_MOD_ALT ? "A" : "-");
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}", key, scanCode,
+                action == GLFW_PRESS     ? "Pressed"
+                : action == GLFW_RELEASE ? "Released"
+                : action == GLFW_REPEAT  ? "Repeat"
+                                         : "Unknown",
+                mods & GLFW_MOD_CONTROL ? "C" : "-",
+                mods & GLFW_MOD_SHIFT ? "S" : "-",
+                mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void OnMouseButton(GLFWwindow *window, int button, int action, int modifer) {
+    auto context = reinterpret_cast<Context *>(glfwGetWindowUserPointer(window));
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+    context->MouseButton(button, action, x, y);
+}
+
+void OnCursorPos(GLFWwindow *window, double x, double y) {
+    auto context = reinterpret_cast<Context *>(glfwGetWindowUserPointer(window));
+    context->MouseMove(x, y);
 }
 
 int main(int argc, const char **argv) {
-  SPDLOG_INFO("START PROGRAM");
+    SPDLOG_INFO("START PROGRAM");
 
-  SPDLOG_INFO("Initialize glfw");
-  if (!glfwInit()) {
-    const char *descrption = nullptr;
-    glfwGetError(&descrption);
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char *descrption = nullptr;
+        glfwGetError(&descrption);
 
-    SPDLOG_ERROR("Failed to intialize glfw: {}", descrption);
-    return -1;
-  }
+        SPDLOG_ERROR("Failed to intialize glfw: {}", descrption);
+        return -1;
+    }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  SPDLOG_INFO("Create glfw window");
-  auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
-                                 nullptr, nullptr);
-  if (!window) {
-    SPDLOG_ERROR("Failed to create glfw window");
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+                                   nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("Failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("Failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
+
+    auto context = Context::Create();
+    if (!context) {
+        SPDLOG_ERROR("Failed to create context.");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSetWindowUserPointer(window, context.get());
+
+    OnFramebufferSizeChanged(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChanged);
+    glfwSetKeyCallback(window, OnKeyEvent);
+    glfwSetMouseButtonCallback(window, OnMouseButton);
+    glfwSetCursorPosCallback(window, OnCursorPos);
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        context->ProcessInput(window);
+        context->Render();
+        glfwSwapBuffers(window);
+    }
+
+    context.reset();
     glfwTerminate();
-    return -1;
-  }
 
-  glfwMakeContextCurrent(window);
-
-  // glad를 활용한 OpenGL 함수 로딩
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    SPDLOG_ERROR("Failed to initialize glad");
-    glfwTerminate();
-    return -1;
-  }
-  auto glVersion = glGetString(GL_VERSION);
-  SPDLOG_INFO("OpenGL context version: {}", glVersion);
-
-  auto context = Context::Create();
-  if (!context) {
-    SPDLOG_ERROR("Failed to create context.");
-    glfwTerminate();
-    return -1;
-  }
-
-  OnFramebufferSizeChanged(window, WINDOW_WIDTH, WINDOW_HEIGHT);
-  glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChanged);
-  glfwSetKeyCallback(window, OnKeyEvent);
-
-  SPDLOG_INFO("Start main loop");
-  while (!glfwWindowShouldClose(window)) {
-    context->Render();
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  context.reset();
-  glfwTerminate();
-
-  return 0;
+    return 0;
 }

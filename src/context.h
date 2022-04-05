26cc6dc5e434582787f38801e30f94780e13ab2c
Sungsik Bae <46157657+Labae@users.noreply.github.com>
2022-04-05 15:12:11 +0900

    add lighting

--- src/context.h
+++ src/context.h
@@ -26,8 +26,6 @@ class Context {
     int m_width{WINDOW_WIDTH};
     int m_height{WINDOW_HEIGHT};
 
-    glm::vec4 m_clearColor{glm::vec4(0.1f, 0.2f, 0.3f, 0.0f)};
-
     VertexLayoutUPtr m_vertexLayout;
     BufferUPtr m_vertexBuffer;
     BufferUPtr m_indexBuffer;
@@ -36,6 +34,7 @@ class Context {
     TextureUPtr m_texture2;
 
     // camera parameters
+    bool m_animaiton{true};
     bool m_cameraControl{false};
     glm::vec2 m_prevMousePos{glm::vec2(0.0f)};
 
@@ -45,6 +44,25 @@ class Context {
 
     float m_cameraPitch{0.0f};
     float m_cameraYaw{0.0f};
+
+    glm::vec4 m_clearColor{glm::vec4(0.1f, 0.2f, 0.3f, 0.0f)};
+
+    struct Light {
+        glm::vec3 position{glm::vec3(3.0f, 3.0f, 3.0f)};
+        glm::vec3 ambient{glm::vec3(0.1f, 0.1f, 0.1f)};
+        glm::vec3 diffuse{glm::vec3(0.5f, 0.5f, 0.5f)};
+        glm::vec3 specular{glm::vec3(1.0f, 1.0f, 1.0f)};
+    };
+    Light m_light;
+
+    // material parameter
+    struct Material {
+        glm::vec3 ambient{glm::vec3(1.0f, 0.5f, 0.3f)};
+        glm::vec3 diffuse{glm::vec3(1.0f, 0.5f, 0.3f)};
+        glm::vec3 specular{glm::vec3(0.5f, 0.5f, 0.5f)};
+        float shininess{32.0f};
+    };
+    Material m_material;
 };
 
 #endif
